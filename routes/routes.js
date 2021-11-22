b49af28986f1f8abe5afbc5f66950bdc60496776
pullejam <79878433+pullejam@users.noreply.github.com>
2021-11-22 10:12:23 -0700

    undone and being reworked

---
+++ routes/routes.js
@@ -0,0 +1,9 @@
+//function that will get the info from the form that the user submits and adds the data to the database
+//in the form of a person model
+exports.createAccount = (req, res) => {
+
+}
+//function used to load the create screen
+exports.loadCreate = (req, res) => {
+
+}
