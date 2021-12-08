22a125eeec5ff5d9d055f24ddaeba907fe7eff69
pullejam <79878433+pullejam@users.noreply.github.com>
2021-12-07 23:30:34 -0700

    Api done

--- canvas.js
+++ canvas.js
@@ -5,6 +5,63 @@ const ctx = canvas.getContext('2d');
 //changing size
 canvas.width = 450;
 canvas.height = 150;
+let Q1A1 = 0;
+let Q1A2 = 0;
+let Q1A3 = 0;
+let Q2A1 = 0;
+let Q2A2 = 0;
+let Q2A3 = 0;
+let Q3A1 = 0;
+let Q3A2 = 0;
+let Q3A3 = 0;
+let url = `http://localHost:3000/getData`
+fetch(url).then(response => response.json())
+    .then(data => {
+        console.log(data);
+
+        data.forEach(element => {
+            switch (element.q1) {
+                case "yes.":
+                    Q1A1++;
+                    break;
+                case "yes!":
+                    Q1A2++;
+                    break;
+                case "Yes?!":
+                    Q1A3++;
+                default:
+                    break;
+            }
+            switch (element.q2) {
+                case "Leaches":
+                    Q2A1++;
+                    break;
+                case "Wasps":
+                    Q2A2++;
+                    break;
+                case "Mosquitoes":
+                    Q2A3++;
+                default:
+                    break;
+            }
+            switch (element.q3) {
+                case "Under the bed":
+                    Q3A1++;
+                    break;
+                case "In a cave":
+                    Q3A2++;
+                    break;
+                case "Nigeria":
+                    Q3A3++;
+                default:
+                    break;
+            }
+        });
+
+
+    }).catch(err => {
+        console.log(err);
+    })
 
 drawText = () => {
     //setting text color
@@ -14,20 +71,20 @@ drawText = () => {
     //setting the text and start location
     //(text, start x, start y)
     //question 1
-    ctx.fillText('Answer 1', 0 , 150);
-    ctx.fillText('Answer 2', 50, 150);
-    ctx.fillText('Answer 3', 100, 150);
-    ctx.fillText(`Question 1`, 50, 10);
+    ctx.fillText('yes.', 0, 150);
+    ctx.fillText('yes!', 50, 150);
+    ctx.fillText('Yes?!', 100, 150);
+    ctx.fillText(`Do you like me?`, 50, 10);
     //question 2
-    ctx.fillText('Answer 1', 150 , 150);
-    ctx.fillText('Answer 2', 200, 150);
-    ctx.fillText('Answer 3', 250, 150);
-    ctx.fillText(`Question 2`, 200, 10);
+    ctx.fillText('Leaches', 150, 150);
+    ctx.fillText('Wasps', 200, 150);
+    ctx.fillText('Mosquitoes', 250, 150);
+    ctx.fillText(`What is your favorite insect`, 200, 10);
     //question3
-    ctx.fillText('Answer 1', 300 , 150);
-    ctx.fillText('Answer 2', 350, 150);
-    ctx.fillText('Answer 3', 400, 150);
-    ctx.fillText(`Question 3`, 350, 10);
+    ctx.fillText('Under the bed', 300, 150);
+    ctx.fillText('In a cave', 350, 150);
+    ctx.fillText('Nigeria', 400, 150);
+    ctx.fillText(`your second grandmas best friends mom birthPlace?`, 350, 10);
 }
 
 drawGraph = (Q1A1, Q1A2, Q1A3, Q2A1, Q2A2, Q2A3, Q3A1, Q3A2, Q3A3) => {
@@ -68,33 +125,33 @@ drawGraph = (Q1A1, Q1A2, Q1A3, Q2A1, Q2A2, Q2A3, Q3A1, Q3A2, Q3A3) => {
 
     //draw rectangles Q1
     //a1
-    ctx.fillRect(10,(1-Q1A1Percent) * canvas.height,20,150);
+    ctx.fillRect(10, (1 - Q1A1Percent) * canvas.height, 20, 150);
     //a2
-    ctx.fillRect(60,(1-Q1A2Percent) * canvas.height,20,150);
+    ctx.fillRect(60, (1 - Q1A2Percent) * canvas.height, 20, 150);
     //a3
-    ctx.fillRect(110,(1-Q1A3Percent) * canvas.height,20,150);
-    
-    ctx.fillRect(142, 0, 5,150);
+    ctx.fillRect(110, (1 - Q1A3Percent) * canvas.height, 20, 150);
+
+    ctx.fillRect(142, 0, 5, 150);
 
     //draw rectangles Q2
     //a1
-    ctx.fillRect(160,(1-Q2A1Percent) * canvas.height,20,150);
+    ctx.fillRect(160, (1 - Q2A1Percent) * canvas.height, 20, 150);
     //a2
-    ctx.fillRect(210,(1-Q2A2Percent) * canvas.height,20,150);
+    ctx.fillRect(210, (1 - Q2A2Percent) * canvas.height, 20, 150);
     //a3
-    ctx.fillRect(260,(1-Q2A3Percent) * canvas.height,20,150);
-    
-    ctx.fillRect(292, 0, 5,150);
+    ctx.fillRect(260, (1 - Q2A3Percent) * canvas.height, 20, 150);
+
+    ctx.fillRect(292, 0, 5, 150);
 
     //draw rectangles Q3
     //a1
-    ctx.fillRect(310,(1-Q3A1Percent) * canvas.height,20,150);
+    ctx.fillRect(310, (1 - Q3A1Percent) * canvas.height, 20, 150);
     //a2
-    ctx.fillRect(360,(1-Q3A2Percent) * canvas.height,20,150);
+    ctx.fillRect(360, (1 - Q3A2Percent) * canvas.height, 20, 150);
     //a3
-    ctx.fillRect(410,(1-Q3A3Percent) * canvas.height,20,150);
+    ctx.fillRect(410, (1 - Q3A3Percent) * canvas.height, 20, 150);
 }
 
 //take in data from API
-drawGraph(10,25,50,50,25,25,50,100,50);
+drawGraph(Q1A1, Q1A2, Q1A3, Q2A1, Q2A2, Q2A3, Q3A1, Q3A2, Q3A3);
 drawText();
--- frontEnd/script.js
+++ frontEnd/script.js
@@ -1,18 +1,170 @@
-
-
-const getInfo = () => {
-    let counter = 0;
+let Q1A1 = 0;
+let Q1A2 = 0;
+let Q1A3 = 0;
+let Q2A1 = 0;
+let Q2A2 = 0;
+let Q2A3 = 0;
+let Q3A1 = 0;
+let Q3A2 = 0;
+let Q3A3 = 0;
+let getData = async () => {
     let url = `http://localHost:3000/getData`
-    fetch(url).then(response => response.json())
+    await fetch(url).then(response => response.json())
         .then(data => {
             console.log(data);
 
             data.forEach(element => {
-                console.log(element.q1)
+                switch (element.q1) {
+                    case "yes.":
+                        Q1A1++;
+                        console.log("match yes.")
+                        break;
+                    case "yes!":
+                        Q1A2++;
+                        break;
+                    case "Yes?!":
+                        Q1A3++;
+                    default:
+                        break;
+                }
+                switch (element.q2) {
+                    case "Leaches":
+                        Q2A1++;
+                        console.log("match leaches")
+                        break;
+                    case "Wasps":
+                        Q2A2++;
+                        break;
+                    case "Mosquitoes":
+                        Q2A3++;
+                    default:
+                        break;
+                }
+                switch (element.q3) {
+                    case "Under the bed":
+                        console.log("match under the bed")
+                        Q3A1++;
+                        break;
+                    case "In a cave":
+                        Q3A2++;
+                        break;
+                    case "Nigeria":
+                        Q3A3++;
+                    default:
+                        break;
+                }
             });
-
-
+            makeGraph();
+            Q1A1 = 0;
+            Q1A2 = 0;
+            Q1A3 = 0;
+            Q2A1 = 0;
+            Q2A2 = 0;
+            Q2A3 = 0;
+            Q3A1 = 0;
+            Q3A2 = 0;
+            Q3A3 = 0;
         }).catch(err => {
             console.log(err);
         })
+}
+let makeGraph = () => {
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+
+    //changing size
+    canvas.width = 400;
+    canvas.height = 150;
+    drawText = () => {
+        //setting text color
+        ctx.fillStyle = '#FFF';
+        //setting font and text size
+        ctx.font = '10px Arial';
+        //setting the text and start location
+        //(text, start x, start y)
+        //question 1
+        ctx.fillText('yes.', 0, 150);
+        ctx.fillText('yes!', 50, 150);
+        ctx.fillText('Yes?!', 100, 150);
+        ctx.fillText(`Do you like me?`, 50, 10);
+        //question 2
+        ctx.fillText('Leaches', 150, 150);
+        ctx.fillText('Wasps', 200, 150);
+        ctx.fillText('Mosquitoes', 250, 150);
+        ctx.fillText(`What is your favorite insect`, 200, 10);
+        //question3
+        ctx.fillText('Under bed', 300, 150);
+        ctx.fillText('In a cave', 350, 150);
+        ctx.fillText('Nigeria', 400, 150);
+        ctx.fillText(`your second grandmas best friends mom birthPlace?`, 350, 10);
+    }
+
+    drawGraph = (Q1A1, Q1A2, Q1A3, Q2A1, Q2A2, Q2A3, Q3A1, Q3A2, Q3A3) => {
+        //get total Q1
+        let Q1total = Q1A1 + Q1A2 + Q1A3;
+        let Q1A1Percent = (Q1A1 / Q1total);
+        let Q1A2Percent = (Q1A2 / Q1total);
+        let Q1A3Percent = (Q1A3 / Q1total);
+
+        let Q2total = Q2A1 + Q2A2 + Q2A3;
+        let Q2A1Percent = (Q2A1 / Q2total);
+        let Q2A2Percent = (Q2A2 / Q2total);
+        let Q2A3Percent = (Q2A3 / Q2total);
+
+        let Q3total = Q3A1 + Q3A2 + Q3A3;
+        let Q3A1Percent = (Q3A1 / Q3total);
+        let Q3A2Percent = (Q3A2 / Q3total);
+        let Q3A3Percent = (Q3A3 / Q3total);
+
+        console.log("Q1A1: " + Q1A1Percent);
+        console.log("Q1A2: " + Q1A2Percent);
+        console.log("Q1A3: " + Q1A3Percent);
+
+        console.log("Q2A1: " + Q2A1Percent);
+        console.log("Q2A2: " + Q2A2Percent);
+        console.log("Q2A3: " + Q2A3Percent);
+
+        console.log("Q3A1: " + Q3A1Percent);
+        console.log("Q3A2: " + Q3A2Percent);
+        console.log("Q3A3: " + Q3A3Percent);
+
+        console.log("Total Q1: " + (Q1A3Percent + Q1A2Percent + Q1A1Percent));
+        console.log("Total Q2: " + (Q2A3Percent + Q2A2Percent + Q2A1Percent));
+        console.log("Total Q3: " + (Q3A3Percent + Q3A2Percent + Q3A1Percent));
+
+        //setting the rect color
+        ctx.fillStyle = '#405';
+
+        //draw rectangles Q1
+        //a1
+        ctx.fillRect(10, (1 - Q1A1Percent) * canvas.height, 20, 150);
+        //a2
+        ctx.fillRect(60, (1 - Q1A2Percent) * canvas.height, 20, 150);
+        //a3
+        ctx.fillRect(110, (1 - Q1A3Percent) * canvas.height, 20, 150);
+
+        ctx.fillRect(142, 0, 5, 150);
+
+        //draw rectangles Q2
+        //a1
+        ctx.fillRect(160, (1 - Q2A1Percent) * canvas.height, 20, 150);
+        //a2
+        ctx.fillRect(210, (1 - Q2A2Percent) * canvas.height, 20, 150);
+        //a3
+        ctx.fillRect(260, (1 - Q2A3Percent) * canvas.height, 20, 150);
+
+        ctx.fillRect(292, 0, 5, 150);
+
+        //draw rectangles Q3
+        //a1
+        ctx.fillRect(310, (1 - Q3A1Percent) * canvas.height, 20, 150);
+        //a2
+        ctx.fillRect(360, (1 - Q3A2Percent) * canvas.height, 20, 150);
+        //a3
+        ctx.fillRect(410, (1 - Q3A3Percent) * canvas.height, 20, 150);
+    }
+
+    //take in data from API
+    drawGraph(Q1A1, Q1A2, Q1A3, Q2A1, Q2A2, Q2A3, Q3A1, Q3A2, Q3A3);
+    drawText();
 }
